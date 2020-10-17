f13bc418e0cc96929c2267389825de7a7ac51c99
I'll Get My Luck! <dmrachan@gmail.com>
2020-10-17 11:08:58 +0700

    Update main.dart
    The changed update to use flag

--- 5/main.dart
+++ 5/main.dart
@@ -1,13 +1,19 @@
 void main() {
-  var list1 = [1,2,3,4];
-  var list2 = [2,4,6,8];
+  var list1 = [1,5,3,7];
+  var list2 = [1,2,4,6,8];
+  int flag;
   for (var getList1 in list1.getRange(0, list1.length)) {
     for (var getList2 in list2.getRange(0, list2.length)) {
       if (getList1 == getList2) {
-        print("There's a list ${getList1} in sublist ${getList2}");
-      } else {
-        print("No!, There's a not list ${getList1} in sublist ${getList2}");
+      flag = 1;
+      continue;
       }
     }
-  } 
+  }
+  
+  if (flag==1) {
+    print("There's a list in a sublist");
+  } else {
+    print("There's a not list in a sublist");
+  }
 }
